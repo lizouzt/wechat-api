dafe2483baec1e3bbca4913227c60dce42d90b55
TangJinwei <tangjw1981@mail.com>
2013-11-19 18:40:23 +0800

    add getMsg method

--- WeChat.class.php
+++ WeChat.class.php
@@ -7,20 +7,100 @@ class WeChat
 	private $_api;
 	private $_errcode;
 	private $_errmsg;
+
 	public function __construct ()
 	{}
+
 	public function getAccessToken ()
 	{
 		$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" .
 		 APPID . "&secret=" . APPSECRET;
 		return $this->_sendRequest($url);
 	}
+	
+	public function getMsg($data){
+	    $msg = $this->_parseMsg($data);
+	    return $msg;
+	}
+
+	public function getTextMsg ()
+	{}
+
+	public function getImageMsg ()
+	{}
+
+	public function getVoiceMsg ()
+	{}
+
+	public function getVideoMsg ()
+	{}
+
+	public function getLocationMsg ()
+	{}
+
+	public function checkSignature ($signature, $timestamp, $nonce)
+	{
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode($tmpArr);
+		$tmpStr = sha1($tmpStr);
+		if ($tmpStr == $signature) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private function _parseMsg ($data)
+	{
+		$_msg = array();
+		$_data = simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA);
+		if (is_object($_data)) {
+			foreach ($_data as $v) {
+				$name = $v->getName();
+				$value = $_data->$name;
+				if ($name == "ToUserName") {
+					$_msg['ToUserName'] = $value;
+				} elseif ($name == "FromUserName") {
+					$_msg['FromUserName'] = $value;
+				} elseif ($name == "CreateTime") {
+					$_msg['CreateTime'] = $value;
+				} elseif ($name == "MsgType") {
+					$_msg['MsgType'] = $value;
+				} elseif ($name == "MsgId") {
+					$_msg['MsgId'] = $value;
+				} elseif ($name == "Content") {
+					$_msg['Content'] = $value;
+				} elseif ($name == "PicUrl") {
+					$_msg['PicUrl'] = $value;
+				} elseif ($name == "MediaId") {
+					$_msg['MediaId'] = $value;
+				} elseif ($name == "Format") {
+					$_msg['Format'] = $value;
+				} elseif ($name == "ThumbMediaId") {
+					$_msg['ThumbMediaId'] = $value;
+				} elseif ($name == "Location_X") {
+					$_msg['Location_X'] = $value;
+				} elseif ($name == "Location_Y") {
+					$_msg['Location_Y'] = $value;
+				} elseif ($name == "Scale") {
+					$_msg['Scale'] = $value;
+				} elseif ($name == "Label") {
+					$_msg['Label'] = $value;
+				}
+			}
+		}
+		return (count($_msg) == 0) ? false : $_msg;
+	}
+
 	private function _sendRequest ($url)
 	{
 		$result = file_get_contents($url);
 		$_result = $this->_parseResult($result);
 		return $_result;
 	}
+
 	private function _parseResult ($result)
 	{
 		$_result = json_decode($result);
@@ -31,6 +111,11 @@ class WeChat
 			return $_result;
 		}
 		return false;
+	}
+
+	public function getError ()
+	{
+		return array('errcode' => $this->_errcode, 'errmsg' => $this->_errmsg);
 	}
 }
 ?>
--- config.php
+++ config.php
@@ -2,4 +2,5 @@
 namespace spacet\wechat;
 const APPID = "wxd790c4c47f8c1eba";
 const APPSECRET = "6c869828b9e712eb2005f589ad81a056";
+const TOKEN = "nfZpV7hz6ZcWSnAB";
 ?>
