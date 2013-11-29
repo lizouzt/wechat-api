5536b42f7996dc2a37977ce78676524df55c003d
TangJinwei <tangjw1981@mail.com>
2013-11-29 14:49:08 +0800

    change constant define method

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ function autoload ($class)
 	require $file_name;
 }
 spl_autoload_register(__NAMESPACE__ . '\autoload');
-const APPID = "wxd790c4c47f8c1eba";
-const APPSECRET = "6c869828b9e712eb2005f589ad81a056";
-const TOKEN = "nfZpV7hz6ZcWSnAB";
+define(APPID, "wxd790c4c47f8c1eba");
+define(APPSECRET, "6c869828b9e712eb2005f589ad81a056");
+define(TOKEN, "nfZpV7hz6ZcWSnAB");
 ?>
