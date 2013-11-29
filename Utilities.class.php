df3bd5b50cd57975387ca3c60f7d44693c7aa262
TangJinwei <tangjw1981@mail.com>
2013-11-29 12:17:05 +0800

    add some curl methods

--- Utilities.class.php
+++ Utilities.class.php
@@ -2,5 +2,19 @@
 namespace spacet\wechat;
 class Utilities
 {
+
+	public function post ($url, $data)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		$result = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_exec($ch);
+		curl_close($ch);
+		if ($result) {
+			return $result;
+		}
+		return false;
+	}
 }
 ?>
