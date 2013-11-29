fb179085204f81f80ab0b85b89dc1fdd9ea68cc5
TangJinwei <tangjw1981@mail.com>
2013-11-29 12:58:40 +0800

    change autoload method

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 namespace spacet\wechat;
 
-function __autoload ($class)
+function autoload ($class)
 {
-	$file_name = "./" . $class . ".class.php";
+	$class = explode('\\', $class);
+	$file_name = end($class) . ".class.php";
 	require $file_name;
 }
+spl_autoload_register(__NAMESPACE__ . '\autoload');
 const APPID = "wxd790c4c47f8c1eba";
 const APPSECRET = "6c869828b9e712eb2005f589ad81a056";
 const TOKEN = "nfZpV7hz6ZcWSnAB";
